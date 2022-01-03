58ab750baf2918c9e8adfacd119b32bed4930a67
SatyaNandikolla <satyanandikolla32@gmail.com>
2022-01-03 12:32:47 +0530

    changes

--- take2_10kcoders/JAVASCRIPT/API_CRUD/FAKE_STORE/getAllUsers.js
+++ take2_10kcoders/JAVASCRIPT/API_CRUD/FAKE_STORE/getAllUsers.js
@@ -1,1 +1,15 @@
-let API_URL='http://localhost:3000/users'
+let API_URL="http://localhost:3000/Users"
+var allUsers=[]
+function getAllUsers(){
+    var getInfo =new XMLHttpRequest();
+    getInfo.onreadystatechange=function(){
+        if(getInfo.readyState ==4 && getInfpo.status ==200){
+            allUsers=JSON.parse(getInfo.response);
+            console.log(allUsers)
+        }
+    }
+    getInfo.open("GET",API_URL);
+    getInfo.send()
+
+}
+getAllUsers()
